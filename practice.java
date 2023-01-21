de6a9353cfc6ae63b00873da2d47fcf8ceaae5b8
이영재 <youngjae9683@gmail.com>
2023-01-21 11:29:34 +0900

    gitpractice

---
+++ practice.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class practice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("안녕하세요");
+        System.out.println("안녕하세요");
+        System.out.println("안녕하세요");
+        System.out.println("안녕하세요");
+        sc.close();
+    }
+}
