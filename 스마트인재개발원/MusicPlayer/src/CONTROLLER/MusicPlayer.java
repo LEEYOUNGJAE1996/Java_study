1be68589a74c4459a68863b4355f5ea2f51753b3
이영재 <youngjae9683@gmail.com>
2023-01-31 15:59:21 +0900

    자바 수업 예제(뮤직플레이어)

--- 스마트인재개발원/MusicPlayer/src/CONTROLLER/MusicPlayer.java
+++ 스마트인재개발원/MusicPlayer/src/CONTROLLER/MusicPlayer.java
@@ -10,16 +10,51 @@ public class MusicPlayer {
     // 재생 / 중지 / 이전 곡 / 다음 곡
 
     // 노래들을 필드로 저장????
-    private ArrayList<Music> musiclist = new ArrayList<Music>();
+    private ArrayList<Music> musicList = new ArrayList<Music>();
+    private int index_1 = 0;
 
     public MusicPlayer() {
         // 기본 생성자를 이용하여 노래 목록 초기화
-        Music music = new Music("HypeBoy", "뉴진스", 120);
-        musiclist.add(music);
+
+        Music music = new Music("HypeBoy", "뉴진스", 46);
+        musicList.add(music);
+        music = new Music("Antifragile", "르세라핌", 36);
+        musicList.add(music);
+        music = new Music("LoveDive", "아이브", 41);
+        musicList.add(music);
+        music = new Music("사건의지평선", "윤하", 48);
+        musicList.add(music);
+        music = new Music("WhenIMove", "카라", 21);
+        musicList.add(music);
+
     }
 
-    public MusicPlayer(Music music) {
-        // 기본 생성자를 이용하여 노래 목록 초기화
-        musiclist.add(music);
+    public Music play() {
+        Music m = musicList.get(index_1);
+        return m;
+    }
+
+    public void stop() {
+
+    }
+
+    public Music previous() {
+        if (index_1 > 0) {
+            index_1--;
+        } else {
+            index_1 = musicList.size() - 1;
+        }
+        Music m = musicList.get(index_1);
+        return m;
+    }
+
+    public Music next() {
+        if (index_1 < musicList.size() - 1) {
+            index_1++;
+        } else {
+            index_1 = 0;
+        }
+        Music m = musicList.get(index_1);
+        return m;
     }
 }
--- 스마트인재개발원/MusicPlayer/src/VIEW/Main.java
+++ 스마트인재개발원/MusicPlayer/src/VIEW/Main.java
@@ -2,6 +2,9 @@ package VIEW;
 
 import java.util.Scanner;
 
+import CONTROLLER.MusicPlayer;
+import MODEL.Music;
+
 //view
 //사용자와 상호작용하여 입력과 결과를 다루는 역할
 public class Main {
@@ -9,17 +12,31 @@ public class Main {
         // 프로그램 구동 및 상호소통(with 사용자)
         Scanner sc = new Scanner(System.in);
         System.out.println("=====Music Player=====");
-
+        MusicPlayer mp = new MusicPlayer();
         while (true) {
-            System.out.println("[1] play [2] pause [3]previous [4]next [5]exit ");
+            System.out.println("[1] Play [2] Pause [3]Previous [4]Next [5]Exit ");
             int choice = sc.nextInt();
             if (choice == 1) {
-
+                // play
+                Music m = mp.play();
+                System.out.println("현재 재생되는 곡 : " + m.getTitle());
+                System.out.println("가수 : " + m.getSinger());
+                System.out.println("play time : " + m.getPlayTime());
             } else if (choice == 2) {
-
+                mp.stop();
+                System.out.println("STOP \tSTOP \tSTOP \tSTOP \tSTOP \tSTOP \tSTOP \tSTOP \t");
+                System.out.println("STOP \tSTOP \tSTOP \tSTOP \tSTOP \tSTOP \tSTOP \tSTOP \t");
+                System.out.println("STOP \tSTOP \tSTOP \tSTOP \tSTOP \tSTOP \tSTOP \tSTOP \t");
             } else if (choice == 3) {
-
+                Music m = mp.previous();
+                System.out.println("현재 재생되는 곡 : " + m.getTitle());
+                System.out.println("가수 : " + m.getSinger());
+                System.out.println("play time : " + m.getPlayTime());
             } else if (choice == 4) {
+                Music m = mp.next();
+                System.out.println("현재 재생되는 곡 : " + m.getTitle());
+                System.out.println("가수 : " + m.getSinger());
+                System.out.println("play time : " + m.getPlayTime());
 
             } else if (choice == 5) {
                 System.exit(0);
